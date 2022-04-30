5f9aaa044113fd628a79b508e169617dd6d6a83a
Lucia Rondan <luciabrondan@gmail.com>
2022-04-30 00:00:22 -0300

    while en js

---
+++ js/main.js
@@ -0,0 +1,6 @@
+let ingreso = prompt ("Ingrese un servicio o ESC")
+while (ingreso != "ESC"){
+    console.log ("El producto ingresado es " + ingreso);
+    ingreso = prompt ("Ingrese otro servicio")
+}
+
